a2b9397e1ae6e9b511d0d6dc802a4a0df299a7ca
DeStep <tankt34.stepanoff@yandex.ru>
2023-05-06 16:27:10 +0300

    Четверка доработали приоритет, но не работает(только одна операция)

--- main.c
+++ main.c
@@ -1,293 +1,229 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <ctype.h>
 #include <string.h>
+#include <math.h>
 
-#define MAX_EXPR_LEN 100
 #define EXPR_SIZE 100
 #define VARIABLE_COUNT 10
 
-// Структура для хранения переменных
 typedef struct {
     char name;
     double value;
 } Variable;
 
-// Структура стека для операций
 typedef struct {
-    char op[MAX_EXPR_LEN];
     int top;
-} Stack;
+    char op[EXPR_SIZE];
+} OpStack;
 
-// Структура стека для чисел
 typedef struct {
-    double num[MAX_EXPR_LEN];
     int top;
+    double num[EXPR_SIZE];
 } NumStack;
 
-// Функция для добавления операции в стек операций
-void push_op(Stack *stack, char op) {
-    stack->op[++stack->top] = op;
-}
 
-// Функция для удаления операции из стека операций
-char pop_op(Stack *stack) {
+char pop_op(OpStack *stack) {
     return stack->op[stack->top--];
 }
 
-// Функция для проверки, является ли символ операцией
-int is_operator(char c) {
-    return c == '+' || c == '-' || c == '*' || c == '/' || c == '^' || c == '%';
-}
-
-// Функция для проверки, является ли символ открывающей скобкой
-int is_left_paren(char c) {
-    return c == '(';
-}
-
-// Функция для проверки, является ли символ закрывающей скобкой
-int is_right_paren(char c) {
-    return c == ')';
-}
-
-// Функция для получения приоритета операции
-int precedence(char op) {
-    switch (op) {
-        case '+':
-        case '-':
-            return 1;
-        case '*':
-        case '/':
-        case '%':
-            return 2;
-        case '^':
-            return 3;
-        default:
-            return -1;
-    }
-}
-
-// Функция для добавления числа в стек чисел
 void push_num(NumStack *stack, double num) {
     stack->num[++stack->top] = num;
 }
 
-// Функция для удаления числа из стека чисел
 double pop_num(NumStack *stack) {
     return stack->num[stack->top--];
 }
 
-// Функция для проверки, является ли символ цифрой
-int is_digit(char c) {
-    return c >= '0' && c <= '9';
+int is_operator(char c) {
+    return c == '+' || c == '-' || c == '*' || c == '/' || c == '%' || c == '^';
 }
 
-// Функция для проверки, является ли символ переменной
+int is_left_paren(char c) {
+    return c == '(';
+}
+
+int is_right_paren(char c) {
+    return c == ')';
+}
+
 int is_variable(char c) {
-    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+    return isalpha(c);
 }
 
-// Функция для получения выражения без пробелов
-void remove_spaces(char *expr) {
+void remove_spaces(char *str) {
     int i, j;
-    for (i = 0, j = 0; expr[i]; i++) {
-        if (expr[i] != ' ') {
-            expr[j++] = expr[i];
+    for (i = 0, j = 0; str[i]; i++) {
+        if (!isspace(str[i])) {
+            str[j++] = str[i];
         }
     }
-    expr[j] = '\0';
+    str[j] = '\0';
 }
 
-// Функция для вычисления значения выражения
+void push_op(OpStack *stack, char op) {
+    NumStack num_stack = { .top = -1 };
+    while (stack->top >= 0 && !is_left_paren(stack->op[stack->top]) &&
+           ((op != '^' && is_operator(stack->op[stack->top])) ||
+            (op == '^' && stack->op[stack->top] == '^') ||
+            (op == '*' && stack->op[stack->top] == '/') ||
+            (op == '/' && stack->op[stack->top] == '*') ||
+            (op == '%' && stack->op[stack->top] == '/') ||
+            (op == '+' && stack->op[stack->top] != '-') ||
+            (op == '-' && stack->op[stack->top] != '+'))) {
+        char popped_op = pop_op(stack);
+        double num2 = pop_num(&num_stack);
+        double num1 = pop_num(&num_stack);
+        switch (popped_op) {
+            case '+':
+                push_num(&num_stack, num1 + num2);
+                break;
+            case '-':
+                push_num(&num_stack, num1 - num2);
+                break;
+            case '*':
+                push_num(&num_stack, num1 * num2);
+                break;
+            case '/':
+                push_num(&num_stack, num1 / num2);
+                break;
+            case '%':
+                push_num(&num_stack, fmod(num1, num2));
+                break;
+            case '^':
+                push_num(&num_stack, pow(num1, num2));
+                break;
+        }
+    }
+    stack->op[++stack->top] = op;
+}
+
+
 double evaluate(char *expr, Variable *variables, int var_count) {
-    Stack op_stack;
-    op_stack.top = -1;
-    NumStack num_stack;
-    num_stack.top = -1;
-    int i;
-    for (i = 0; expr[i]; i++) {
-        if (is_digit(expr[i])) {
-            // Если символ - цифра, добавляем число в стек чисел
-            double num = 0.0;
-            while (is_digit(expr[i])) {
-                num = num * 10.0 + (expr[i] - '0');
+    OpStack op_stack = { .top = -1 };
+    NumStack num_stack = { .top = -1 };
+    int i, j;
+    for (i = 0, j = 0; expr[i]; i++) {
+        if (isdigit(expr[i]) || (expr[i] == '.')) {
+            double num = strtod(&expr[i], NULL);
+            push_num(&num_stack, num);
+            while (isdigit(expr[i]) || (expr[i] == '.')) {
                 i++;
-            }
-            if (expr[i] == '.') {
-                i++;
-                double frac = 1.0;
-                while (is_digit(expr[i])) {
-                    frac /= 10.0;
-                    num += frac * (expr[i] - '0');
-                    i++;
-                }
             }
             i--;
-            push_num(&num_stack, num);
         } else if (is_variable(expr[i])) {
-            // Если символ - переменная, добавляем значение переменной в стек чисел
-            int j;
-            for (j = 0; j < var_count; j++) {
-                if (variables[j].name == expr[i]) {
-                    push_num(&num_stack, variables[j].value);
+            int k;
+            for (k = 0; k < var_count; k++) {
+                if (variables[k].name == expr[i]) {
                     break;
                 }
             }
-            if (j == var_count) {
-                printf("Enter value for variable %c: ", expr[i]);
-                double value;
-                scanf("%lf", &value);
-                variables[var_count].name = expr[i];
-                variables[var_count].value = value;
-                var_count++;
-                push_num(&num_stack, value);
+            if (k == var_count) {
+                printf("Error: undefined variable '%c'\n", expr[i]);
+                exit(EXIT_FAILURE);
             }
+            push_num(&num_stack, variables[k].value);
         } else if (is_operator(expr[i])) {
-            // Если символ - операция, добавляем ее в стек операций
-            while (op_stack.top >= 0 && precedence(op_stack.op[op_stack.top]) >= precedence(expr[i])) {
-                char op = pop_op(&op_stack);
-                double num2 = pop_num(&num_stack);
-                double num1 = pop_num(&num_stack);
-                double result;
-                switch (op) {
-                    case '+':
-                        result = num1 + num2;
-                        break;
-                    case '-':
-                        result = num1 - num2;
-                        break;
-                    case '*':
-                        result = num1 * num2;
-                        break;
-                    case '/':
-                        result = num1 / num2;
-                        break;
-                    case '%':
-                        result = fmod(num1, num2);
-                        break;
-                    case '^':
-                        result = pow(num1, num2);
-                        break;
-                }
-                push_num(&num_stack, result);
-            }
             push_op(&op_stack, expr[i]);
         } else if (is_left_paren(expr[i])) {
-            // Если символ - открывающая скобка, добавляем ее в стек операций
             push_op(&op_stack, expr[i]);
         } else if (is_right_paren(expr[i])) {
-            // Если символ - закрывающая скобка, выполняем операции до открывающей скобки
             while (op_stack.top >= 0 && !is_left_paren(op_stack.op[op_stack.top])) {
                 char op = pop_op(&op_stack);
                 double num2 = pop_num(&num_stack);
                 double num1 = pop_num(&num_stack);
-                double result;
                 switch (op) {
                     case '+':
-                        result = num1 + num2;
+                        push_num(&num_stack, num1 + num2);
                         break;
                     case '-':
-                        result = num1 - num2;
+                        push_num(&num_stack, num1 - num2);
                         break;
                     case '*':
-                        result = num1 * num2;
+                        push_num(&num_stack, num1 * num2);
                         break;
                     case '/':
-                        result = num1 / num2;
+                        push_num(&num_stack, num1 / num2);
                         break;
                     case '%':
-                        result = fmod(num1, num2);
+                        push_num(&num_stack, fmod(num1, num2));
                         break;
                     case '^':
-                        result = pow(num1, num2);
+                        push_num(&num_stack, pow(num1, num2));
                         break;
                 }
-                push_num(&num_stack, result);
             }
-            if (op_stack.top >= 0 && is_left_paren(op_stack.op[op_stack.top])) {
-                pop_op(&op_stack);
-            } else {
+            if (op_stack.top < 0) {
                 printf("Error: mismatched parentheses\n");
                 exit(EXIT_FAILURE);
             }
+            pop_op(&op_stack);
         } else {
             printf("Error: invalid character '%c'\n", expr[i]);
             exit(EXIT_FAILURE);
         }
     }
-
-// Выполняем оставшиеся операции в стеке операций
     while (op_stack.top >= 0) {
+        if (is_left_paren(op_stack.op[op_stack.top])) {
+            printf("Error: mismatched parentheses\n");
+            exit(EXIT_FAILURE);
+        }
         char op = pop_op(&op_stack);
         double num2 = pop_num(&num_stack);
         double num1 = pop_num(&num_stack);
-        double result;
         switch (op) {
             case '+':
-                result = num1 + num2;
+                push_num(&num_stack, num1 + num2);
                 break;
             case '-':
-                result = num1 - num2;
+                push_num(&num_stack, num1 - num2);
                 break;
             case '*':
-                result = num1 * num2;
+                push_num(&num_stack, num1 * num2);
                 break;
             case '/':
-                result = num1 / num2;
+                push_num(&num_stack, num1 / num2);
                 break;
             case '%':
-                result = fmod(num1, num2);
+                push_num(&num_stack, fmod(num1, num2));
                 break;
             case '^':
-                result = pow(num1, num2);
+                push_num(&num_stack, pow(num1, num2));
                 break;
         }
-        push_num(&num_stack, result);
     }
-
-// Возвращаем значение выражения
-    return pop_num(&num_stack);
+    if (num_stack.top != 0) {
+        printf("Error: invalid expression\n");
+        exit(EXIT_FAILURE);
+    }
+    return num_stack.num[num_stack.top];
 }
 
 int main() {
     char expr[EXPR_SIZE];
-    printf("Enter an arithmetic expression: ");
-    fgets(expr, EXPR_SIZE, stdin);
-    // Убираем лишние пробелы из выражения
-    remove_spaces(expr);
-
-// Определяем переменные в выражении
     Variable variables[VARIABLE_COUNT];
     int var_count = 0;
+    printf("Enter expression: ");
+    fgets(expr, EXPR_SIZE, stdin);
+    remove_spaces(expr);
     int i;
     for (i = 0; expr[i]; i++) {
         if (is_variable(expr[i])) {
             int j;
             for (j = 0; j < var_count; j++) {
-                if (variables[j].name == expr[i]) {
+                if (expr[i] == variables[j].name) {
                     break;
                 }
             }
             if (j == var_count) {
                 variables[var_count].name = expr[i];
-                variables[var_count].value = 0.0;
+                printf("Enter value for variable %c: ", variables[var_count].name);
+                scanf("%lf", &variables[var_count].value);
                 var_count++;
             }
         }
     }
-
-// Вводим значения переменных
-    for (i = 0; i < var_count; i++) {
-        printf("Enter value for variable %c: ", variables[i].name);
-        scanf("%lf", &variables[i].value);
-    }
-
-// Вычисляем значение выражения
     double result = evaluate(expr, variables, var_count);
-
-// Выводим результат
     printf("Result: %g\n", result);
-
     return 0;
 }
-
