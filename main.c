0cdda8f366d74def1e2d2f7693cb756bca377c42
DeStep <tankt34.stepanoff@yandex.ru>
2023-05-09 21:13:11 +0300

    Четверка, работает приоритет, но СУКА НЕ РАБОТАЮТ ПЕРЕМЕННЫЕ

--- main.c
+++ main.c
@@ -1,229 +1,201 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
-#include <math.h>
+#include <ctype.h>
+#include <math.h>()
 
-#define EXPR_SIZE 100
-#define VARIABLE_COUNT 10
+#define MAX_EXPR_LEN 100
+#define MAX_VAR_LEN 10
 
 typedef struct {
-    char name;
+    char* name;
     double value;
 } Variable;
 
 typedef struct {
-    int top;
-    char op[EXPR_SIZE];
-} OpStack;
+    char op;
+    int priority;
+} Operator;
 
-typedef struct {
-    int top;
-    double num[EXPR_SIZE];
-} NumStack;
+const Operator operators[] = {
+        {'+', 1},
+        {'-', 1},
+        {'*', 2},
+        {'/', 2},
+        {'%', 2},
+        {'^', 3}
+};
 
-
-char pop_op(OpStack *stack) {
-    return stack->op[stack->top--];
-}
-
-void push_num(NumStack *stack, double num) {
-    stack->num[++stack->top] = num;
-}
-
-double pop_num(NumStack *stack) {
-    return stack->num[stack->top--];
-}
+const int num_operators = sizeof(operators) / sizeof(Operator);
+Variable vars[MAX_VAR_LEN];
+int num_vars = 0;
 
 int is_operator(char c) {
-    return c == '+' || c == '-' || c == '*' || c == '/' || c == '%' || c == '^';
-}
-
-int is_left_paren(char c) {
-    return c == '(';
-}
-
-int is_right_paren(char c) {
-    return c == ')';
-}
-
-int is_variable(char c) {
-    return isalpha(c);
-}
-
-void remove_spaces(char *str) {
-    int i, j;
-    for (i = 0, j = 0; str[i]; i++) {
-        if (!isspace(str[i])) {
-            str[j++] = str[i];
+    int i;
+    for (i = 0; i < num_operators; i++) {
+        if (operators[i].op == c) {
+            return 1;
         }
     }
-    str[j] = '\0';
+    return 0;
 }
 
-void push_op(OpStack *stack, char op) {
-    NumStack num_stack = { .top = -1 };
-    while (stack->top >= 0 && !is_left_paren(stack->op[stack->top]) &&
-           ((op != '^' && is_operator(stack->op[stack->top])) ||
-            (op == '^' && stack->op[stack->top] == '^') ||
-            (op == '*' && stack->op[stack->top] == '/') ||
-            (op == '/' && stack->op[stack->top] == '*') ||
-            (op == '%' && stack->op[stack->top] == '/') ||
-            (op == '+' && stack->op[stack->top] != '-') ||
-            (op == '-' && stack->op[stack->top] != '+'))) {
-        char popped_op = pop_op(stack);
-        double num2 = pop_num(&num_stack);
-        double num1 = pop_num(&num_stack);
-        switch (popped_op) {
-            case '+':
-                push_num(&num_stack, num1 + num2);
-                break;
-            case '-':
-                push_num(&num_stack, num1 - num2);
-                break;
-            case '*':
-                push_num(&num_stack, num1 * num2);
-                break;
-            case '/':
-                push_num(&num_stack, num1 / num2);
-                break;
-            case '%':
-                push_num(&num_stack, fmod(num1, num2));
-                break;
-            case '^':
-                push_num(&num_stack, pow(num1, num2));
-                break;
+int get_operator_priority(char op) {
+    int i;
+    for (i = 0; i < num_operators; i++) {
+        if (operators[i].op == op) {
+            return operators[i].priority;
         }
     }
-    stack->op[++stack->top] = op;
+    return 0;
 }
 
+int is_variable(char* str) {
+    int i;
+    for (i = 0; i < num_vars; i++) {
+        if (strcmp(vars[i].name, str) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
 
-double evaluate(char *expr, Variable *variables, int var_count) {
-    OpStack op_stack = { .top = -1 };
-    NumStack num_stack = { .top = -1 };
-    int i, j;
-    for (i = 0, j = 0; expr[i]; i++) {
-        if (isdigit(expr[i]) || (expr[i] == '.')) {
-            double num = strtod(&expr[i], NULL);
-            push_num(&num_stack, num);
-            while (isdigit(expr[i]) || (expr[i] == '.')) {
+double get_variable_value(char* str) {
+    int i;
+    for (i = 0; i < num_vars; i++) {
+        if (strcmp(vars[i].name, str) == 0) {
+            return vars[i].value;
+        }
+    }
+    return 0;
+}
+
+void set_variable_value(char* str, double value) {
+    int i;
+    for (i = 0; i < num_vars; i++) {
+        if (strcmp(vars[i].name, str) == 0) {
+            vars[i].value = value;
+            return;
+        }
+    }
+    vars[num_vars].name = strdup(str);
+    vars[num_vars].value = value;
+    num_vars++;
+}
+
+double calculate(double a, double b, char op) {
+    switch (op) {
+        case '+': return a + b;
+        case '-': return a - b;
+        case '*': return a * b;
+        case '/': return a / b;
+        case '%': return (int)a % (int)b;
+        case '^': return pow(a, b);
+    }
+    return 0;
+}
+
+double evaluate_expression(char* expr) {
+    double num_stack[MAX_EXPR_LEN];
+    char op_stack[MAX_EXPR_LEN];
+    int num_top = -1;
+    int op_top = -1;
+    int i;
+
+    for (i = 0; expr[i] != '\0'; i++) {
+        if (isdigit(expr[i])) {
+            double num = expr[i] - '0';
+            while (isdigit(expr[i + 1])) {
+                num = num * 10 + (expr[i + 1] - '0');
                 i++;
             }
+            num_top++;
+            num_stack[num_top] = num;
+        } else if (isalpha(expr[i])) {
+            char var[MAX_VAR_LEN];
+            int j = 0;
+            while (isalpha(expr[i]) || isdigit(expr[i])) {
+                var[j] = expr[i];
+                i++;
+                j++;
+            }
+            var[j] = '\0';
+            if (is_variable(var)) {
+                double num = get_variable_value(var);
+                num_top++;
+                num_stack[num_top] = num;
+            } else {
+                printf("Undefined variable: %s\n", var);
+                exit(1);
+            }
             i--;
-        } else if (is_variable(expr[i])) {
-            int k;
-            for (k = 0; k < var_count; k++) {
-                if (variables[k].name == expr[i]) {
-                    break;
-                }
+        } else if (expr[i] == '(') {
+            op_top++;
+            op_stack[op_top] = expr[i];
+        } else if (expr[i] == ')') {
+            while (op_top >= 0 && op_stack[op_top] != '(') {
+                double b = num_stack[num_top];
+                num_top--;
+                double a = num_stack[num_top];
+                num_top--;
+                char op = op_stack[op_top];
+                op_top--;
+                double result = calculate(a, b, op);
+                num_top++;
+                num_stack[num_top] = result;
             }
-            if (k == var_count) {
-                printf("Error: undefined variable '%c'\n", expr[i]);
-                exit(EXIT_FAILURE);
-            }
-            push_num(&num_stack, variables[k].value);
+            op_top--;
         } else if (is_operator(expr[i])) {
-            push_op(&op_stack, expr[i]);
-        } else if (is_left_paren(expr[i])) {
-            push_op(&op_stack, expr[i]);
-        } else if (is_right_paren(expr[i])) {
-            while (op_stack.top >= 0 && !is_left_paren(op_stack.op[op_stack.top])) {
-                char op = pop_op(&op_stack);
-                double num2 = pop_num(&num_stack);
-                double num1 = pop_num(&num_stack);
-                switch (op) {
-                    case '+':
-                        push_num(&num_stack, num1 + num2);
-                        break;
-                    case '-':
-                        push_num(&num_stack, num1 - num2);
-                        break;
-                    case '*':
-                        push_num(&num_stack, num1 * num2);
-                        break;
-                    case '/':
-                        push_num(&num_stack, num1 / num2);
-                        break;
-                    case '%':
-                        push_num(&num_stack, fmod(num1, num2));
-                        break;
-                    case '^':
-                        push_num(&num_stack, pow(num1, num2));
-                        break;
-                }
+            char op = expr[i];
+            while (op_top >= 0 && op_stack[op_top] != '(' &&
+                   get_operator_priority(op_stack[op_top]) >= get_operator_priority(op)) {
+                double b = num_stack[num_top];
+                num_top--;
+                double a = num_stack[num_top];
+                num_top--;
+                char op2 = op_stack[op_top];
+                op_top--;
+                double result = calculate(a, b, op2);
+                num_top++;
+                num_stack[num_top] = result;
             }
-            if (op_stack.top < 0) {
-                printf("Error: mismatched parentheses\n");
-                exit(EXIT_FAILURE);
+            op_top++;
+            op_stack[op_top] = op;
+        } else if (expr[i] == '=') {
+            char var[MAX_VAR_LEN];
+            int j = 0;
+            while (isalpha(expr[i + 1]) || isdigit(expr[i + 1])) {
+                var[j] = expr[i + 1];
+                i++;
+                j++;
             }
-            pop_op(&op_stack);
-        } else {
-            printf("Error: invalid character '%c'\n", expr[i]);
-            exit(EXIT_FAILURE);
+            var[j] = '\0';
+            double value = evaluate_expression(expr + i + 2);
+            set_variable_value(var, value);
+            return value;
         }
     }
-    while (op_stack.top >= 0) {
-        if (is_left_paren(op_stack.op[op_stack.top])) {
-            printf("Error: mismatched parentheses\n");
-            exit(EXIT_FAILURE);
-        }
-        char op = pop_op(&op_stack);
-        double num2 = pop_num(&num_stack);
-        double num1 = pop_num(&num_stack);
-        switch (op) {
-            case '+':
-                push_num(&num_stack, num1 + num2);
-                break;
-            case '-':
-                push_num(&num_stack, num1 - num2);
-                break;
-            case '*':
-                push_num(&num_stack, num1 * num2);
-                break;
-            case '/':
-                push_num(&num_stack, num1 / num2);
-                break;
-            case '%':
-                push_num(&num_stack, fmod(num1, num2));
-                break;
-            case '^':
-                push_num(&num_stack, pow(num1, num2));
-                break;
-        }
+
+    while (op_top >= 0) {
+        double b = num_stack[num_top];
+        num_top--;
+        double a = num_stack[num_top];
+        num_top--;
+        char op = op_stack[op_top];
+        op_top--;
+        double result = calculate(a, b, op);
+        num_top++;
+        num_stack[num_top] = result;
     }
-    if (num_stack.top != 0) {
-        printf("Error: invalid expression\n");
-        exit(EXIT_FAILURE);
-    }
-    return num_stack.num[num_stack.top];
+
+    return num_stack[num_top];
 }
 
 int main() {
-    char expr[EXPR_SIZE];
-    Variable variables[VARIABLE_COUNT];
-    int var_count = 0;
+    char expr[MAX_EXPR_LEN];
     printf("Enter expression: ");
-    fgets(expr, EXPR_SIZE, stdin);
-    remove_spaces(expr);
-    int i;
-    for (i = 0; expr[i]; i++) {
-        if (is_variable(expr[i])) {
-            int j;
-            for (j = 0; j < var_count; j++) {
-                if (expr[i] == variables[j].name) {
-                    break;
-                }
-            }
-            if (j == var_count) {
-                variables[var_count].name = expr[i];
-                printf("Enter value for variable %c: ", variables[var_count].name);
-                scanf("%lf", &variables[var_count].value);
-                var_count++;
-            }
-        }
-    }
-    double result = evaluate(expr, variables, var_count);
-    printf("Result: %g\n", result);
+    fgets(expr, MAX_EXPR_LEN, stdin);
+    double result = evaluate_expression(expr);
+    printf("Result: %lf\n", result);
     return 0;
 }
