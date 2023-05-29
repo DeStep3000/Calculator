fcca41302a1d65d581423b0a8580da98e00c3920
DeStep <tankt34.stepanoff@yandex.ru>
2023-05-29 17:59:34 +0300

    Недоработанная версия с комплексными числами

--- main.c
+++ main.c
@@ -1,320 +1,206 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <string.h>
 #include <math.h>
 #include <complex.h>
 
-// Функция, возвращающая значение выражения, заданного строкой expr,
-// с учетом значений переменных, заданных в словаре vars
-double complex eval(char *expr, double complex *vars) {
-    int n = strlen(expr);
-    char op;
-    double complex val1, val2;
-    int i, j, k, l;
+#define MAX_EXPR_LEN 1000
+#define MAX_VAR_NAME_LEN 50
+#define MAX_VARS 100
 
-    // Стек для хранения значений выражений в скобках
-    double complex stack[100];
-    int top = -1;
+struct Variable {
+    char name[MAX_VAR_NAME_LEN];
+    complex double value;
+};
 
-    // Проходим по строке и вычисляем выражение
-    for (i = 0; i < n; i++) {
-        // Если символ является буквой, считаем, что это имя переменной или функции
-        if (expr[i] >= 'a' && expr[i] <= 'z') {
-            char name[20] = "";
-            j = i;
-            while (j < n && ((expr[j] >= 'a' && expr[j] <= 'z') || (expr[j] >= '0' && expr[j] <= '9'))) {
-                strncat(name, &expr[j], 1);
-                j++;
-            }
-            i = j - 1;
+struct Variable vars[MAX_VARS];
+int num_vars = 0;
 
-            // Если это имя функции, вычисляем ее значение и помещаем в стек
-            if (strcmp(name, "cos") == 0) {
-                if (expr[i + 1] != '(') {
-                    printf("Ошибка: ожидалась открывающая скобка после имени функции \"cos\"\n");
-                    exit(1);
-                }
-                j = i + 2;
-                k = j;
-                while (k < n && expr[k] != ')') {
-                    k++;
-                }
-                if (k >= n) {
-                    printf("Ошибка: не найдена закрывающая скобка в вызове функции \"cos\"\n");
-                    exit(1);
-                }
-                char arg[100] = "";
-                strncpy(arg, &expr[j], k - j);
-                double complex argval = eval(arg, vars);
-                double complex result = cos(argval);
-                stack[++top] = result;
-                i = k;
-            } else if (strcmp(name, "sin") == 0) {
-                if (expr[i + 1] != '(') {
-                    printf("Ошибка: ожидалась открывающая скобка после имени функции \"sin\"\n");
-                    exit(1);
-                }
-                j = i + 2;
-                k = j;
-                while (k < n && expr[k] != ')') {
-                    k++;
-                }
-                if (k >= n) {
-                    printf("Ошибка: не найдена закрывающая скобка в вызове функции \"sin\"\n");
-                    exit(1);
-                }
-                char arg[100] = "";
-                strncpy(arg, &expr[j], k - j);
-                double complex argval = eval(arg, vars);
-                double complex result = sin(argval);
-                stack[++top] = result;
-                i = k;
-            } else if (strcmp(name, "tg") == 0) {// Функция tg еще не реализована
-                printf("Функция tg не реализована\n");
-                exit(1);
-            } else if (strcmp(name, "log") == 0) {
-                if (expr[i + 1] != '(') {
-                    printf("Ошибка: ожидалась открывающая скобка после имени функции 'log'\n");
-                    exit(1);
-                }
-                j = i + 2;
-                k = j;
-                while (k < n && expr[k] != ',') {
-                    k++;
-                }
-                if (k >= n) {
-                    printf("Ошибка: не найдена запятая в вызове функции 'log'\n");
-                    exit(1);
-                }
-                char arg1[100] = "";
-                strncpy(arg1, &expr[j], k - j);
-                double complex arg1val = eval(arg1, vars);
-                j = k + 1;
-                k = j;
-                while (k < n && expr[k] != ')') {
-                    k++;
-                }
-                if (k >= n) {
-                    printf("Ошибка: не найдена закрывающая скобка в вызове функции 'log'\n");
-                    exit(1);
-                }
-                char arg2[100] = "";
-                strncpy(arg2, &expr[j], k - j);
-                double complex arg2val = eval(arg2, vars);
-                double complex result = clog(arg1val) / clog(arg2val);
-                stack[++top] = result;
-                i = k;
-            } else if (strcmp(name, "ln") == 0) {
-                if (expr[i + 1] != '(') {
-                    printf("Ошибка: ожидалась открывающая скобка после имени функции 'ln'\n");
-                    exit(1);
-                }
-                j = i + 2;
-                k = j;
-                while (k < n && expr[k] != ')') {
-                    k++;
-                }
-                if (k >= n) {
-                    printf("Ошибка: не найдена закрывающая скобка в вызове функции 'ln'\n");
-                    exit(1);
-                }
-                char arg[100] = "";
-                strncpy(arg, &expr[j], k - j);
-                double complex argval = eval(arg, vars);
-                double complex result = clog(argval);
-                stack[++top] = result;
-                i = k;
-            } else if (strcmp(name, "sqrt") == 0) {
-                if (expr[i + 1] != '(') {
-                    printf("Ошибка: ожидалась открывающая скобка после имени функции 'sqrt'\n");
-                    exit(1);
-                }
-                j = i + 2;
-                k = j;
-                while (k < n && expr[k] != ')') {
-                    k++;
-                }
-                if (k >= n) {
-                    printf("Ошибка: не найдена закрывающая скобка в вызове функции 'sqrt'\n");
-                    exit(1);
-                }
-                char arg[100] = "";
-                strncpy(arg, &expr[j], k - j);
-                double complex argval = eval(arg, vars);
-                double complex result = csqrt(argval);
-                stack[++top] = result;
-                i = k;
-            } else if (strcmp(name, "pow") == 0) {
-                // Функция pow еще не реализована
-                printf("Функция pow не реализована\n");
-                exit(1);
-            } else if (strcmp(name, "abs") == 0) {
-                if (expr[i + 1] != '(') {
-                    printf("Ошибка: ожидалась открывающая скобка после имени функции 'abs'\n");
-                    exit(1);
-                }
-                j = i + 2;
-                k = j;
-                while (k < n && expr[k] != ')') {
-                    k++;
-                }
-                if (k >= n) {
-                    printf("Ошибка: не найдена закрывающая скобка в вызове функции 'abs'\n");
-                    exit(1);
-                }
-                char arg[100] = "";
-                strncpy(arg, &expr[j], k - j);
-                double complex argval = eval(arg, vars);
-                double result = cabs(argval);
-                stack[++top] = result;
-                i = k;
-            } else if (strcmp(name, "exp") == 0) {
-                if (expr[i + 1] != '(') {
-                    printf("Ошибка: ожидалась открывающая скобка после имени функции 'exp'\n");
-                    exit(1);
-                }
-                j = i + 2;
-                k = j;
-                while (k < n && expr[k] != ')') {
-                    k++;
-                }
-                if (k >= n) {
-                    printf("Ошибка: не найдена закрывающая скобка в вызове функции 'exp'\n");
-                    exit(1);
-                }
-                char arg[100] = "";
-                strncpy(arg, &expr[j], k - j);
-                double complex argval = eval(arg, vars);
-                double complex result = cexp(argval);
-                stack[++top] = result;
-                i = k;
-            } else if (strcmp(name, "real") == 0) {
-                if (expr[i + 1] != '(') {
-                    printf("Ошибка: ожидалась открывающая скобка после имени функции 'real'\n");
-                    exit(1);
-                }
-                j = i + 2;
-                k = j;
-                while (k < n && expr[k] != ')') {
-                    k++;
-                }
-                if (k >= n) {
-                    printf("Ошибка: не найдена закрывающая скобка в вызове функции 'real'\n");
-                    exit(1);
-                }
-                char arg[100] = "";
-                strncpy(arg, &expr[j], k - j);
-                double complex argval = eval(arg, vars);
-                double result = creal(argval);
-                stack[++top] = result;
-                i = k;
-            } else if (strcmp(name, "imag") == 0) {
-                if (expr[i + 1] != '(') {
-                    printf("Ошибка: ожидалась открывающая скобка после имени функции 'imag'\n");
-                    exit(1);
-                }
-                j = i + 2;
-                k = j;
-                while (k < n && expr[k] != ')') {
-                    k++;
-                }
-                if (k >= n) {
-                    printf("Ошибка: не найдена закрывающая скобка в вызове функции 'imag'\n");
-                    exit(1);
-                    char arg[100] = "";
-                    strncpy(arg, &expr[j], k - j);
-                    double complex argval = eval(arg, vars);
-                    double result = cimag(argval);
-                    stack[++top] = result;
-                    i = k;
-                } else if (strcmp(name, "mag") == 0) {
-                    if (expr[i + 1] != '(') {
-                        printf("Ошибка: ожидалась открывающая скобка после имени функции 'mag'\n");
-                        exit(1);
-                    }
-                    j = i + 2;
-                    k = j;
-                    while (k < n && expr[k] != ')') {
-                        k++;
-                    }
-                    if (k >= n) {
-                        printf("Ошибка: не найдена закрывающая скобка в вызове функции 'mag'\n");
-                        exit(1);
-                    }
-                    char arg[100] = "";
-                    strncpy(arg, &expr[j], k - j);
-                    double complex argval = eval(arg, vars);
-                    double result = cabs(argval);
-                    stack[++top] = result;
-                    i = k;
-                } else if (strcmp(name, "phase") == 0) {
-                    if (expr[i + 1] != '(') {
-                        printf("Ошибка: ожидалась открывающая скобка после имени функции 'phase'\n");
-                        exit(1);
-                    }
-                    j = i + 2;
-                    k = j;
-                    while (k < n && expr[k] != ')') {
-                        k++;
-                    }
-                    if (k >= n) {
-                        printf("Ошибка: не найдена закрывающая скобка в вызове функции 'phase'\n");
-                        exit(1);
-                    }
-                    char arg[100] = "";
-                    strncpy(arg, &expr[j], k - j);
-                    double complex argval = eval(arg, vars);
-                    double result = carg(argval);
-                    stack[++top] = result;
-                    i = k;
-                } else {
-                    printf("Ошибка: неизвестная функция '%s'\n", name);
-                    exit(1);
-                }
-            } else {
-// Ошибка: неизвестный символ
-                printf("Ошибка: неизвестный символ '%c'\n", expr[i]);
-                exit(1);
-            }
-        }
-        // Проверяем, что в стеке осталось ровно одно значение - результат вычислений
-        if (top != 0) {
-            printf("Ошибка: некорректное выражение\n");
-            exit(1);
-        }
 
-// Возвращаем результат вычислений
-        return stack[0];
+int is_operator(char c) {
+    return c == '+' || c == '-' || c == '*' || c == '/' || c == '^' || c == '%';
+}
+
+int get_operator_precedence(char op) {
+    switch (op) {
+        case '+':
+        case '-':
+            return 1;
+        case '*':
+        case '/':
+        case '%':
+            return 2;
+        case '^':
+            return 3;
+        default:
+            return -1;
     }
 }
 
-int main() {
-// Задаем начальные значения переменных
-    struct var vars[] = {
-            {"len", 191.76},
-            {"v0",  299.799},
-            {"S11", -0.01 - 0.92I},
-            {"S22", 0.0001 + 0.997I},
-            {NULL,  0.0}
-    };
+int is_valid_name_char(char c, int i) {
+    return isalnum(c) || (i > 0 && c == '_');
+}
 
-// Считываем выражение с клавиатуры
-    char expr[1000];
-    printf("Введите выражение: ");
-    fgets(expr, sizeof(expr), stdin);
-
-// Удаляем символ перевода строки из введенной строки, если он есть
-    int n = strlen(expr);
-    if (n > 0 && expr[n - 1] == '\n') {
-        expr[n - 1] = '\0';
-        n--;
+int is_valid_name(char *name) {
+    if (!isalpha(name[0])) {
+        return 0;
     }
+    int len = strlen(name);
+    for (int i = 1; i < len; i++) {
+        if (!is_valid_name_char(name[i], i)) {
+            return 0;
+        }
+    }
+    return 1;
+}
 
-// Вычисляем значение выражения
-    double complex result = eval(expr, vars);
+complex double get_variable_value(char *name) {
+    for (int i = 0; i < num_vars; i++) {
+        if (strcmp(vars[i].name, name) == 0) {
+            return vars[i].value;
+        }
+    }
+    printf("Undefined variable: %s\n", name);
+    return NAN;
+}
 
-// Выводим результат
-    printf("Результат: %.5f%+.5fj\n", creal(result), cimag(result));
+void set_variable_value(char *name, complex double value) {
+    for (int i = 0; i < num_vars; i++) {
+        if (strcmp(vars[i].name, name) == 0) {
+            vars[i].value = value;
+            return;
+        }
+    }
+    if (num_vars >= MAX_VARS) {
+        printf("Too many variables\n");
+        return;
+    }
+    if (!is_valid_name(name)) {
+        printf("Invalid variable name: %s\n", name);
+        return;
+    }
+    struct Variable var;
+    strcpy(var.name, name);
+    var.value = value;
+    vars[num_vars++] = var;
+}
 
+complex double apply_operator(char op, complex double x, complex double y) {
+    switch (op) {
+        case '+':
+            return x + y;
+        case '-':
+            return x - y;
+        case '*':
+            return x * y;
+        case '/':
+            return x / y;
+        case '%':
+            return fmod(x, y);
+        case '^':
+            return cpow(x, y);
+        default:
+            return NAN;
+    }
+}
+
+complex double evaluate_expression(char *expr) {
+    int operator_top = -1;
+    char operator_stack[MAX_EXPR_LEN];
+    complex double value_stack[MAX_EXPR_LEN];
+    int value_top = -1;
+    int i = 0;
+    int unary_minus = 0;
+    while (expr[i] != '\0' && expr[i] != '\n') {
+        if (isspace(expr[i])) {
+            i++;
+        } else if (isdigit(expr[i])) {
+            char *endptr;
+            double value = strtod(&expr[i], &endptr);
+            value_stack[++value_top] = unary_minus ? -value : value;
+            unary_minus = 0;
+            i = endptr - expr;
+        }
+        else if (isalpha(expr[i])) {
+            char name[MAX_VAR_NAME_LEN];
+            int j = 0;
+            while (is_valid_name_char(expr[i], j)) {
+                name[j++] = expr[i++];
+            }
+            name[j] = '\0';
+            if (strcmp(name, "PI") == 0) {
+                value_stack[++value_top] = unary_minus ? -M_PI : M_PI;
+            } else if (strcmp(name, "e") == 0) {
+                value_stack[++value_top] = unary_minus ? -M_E : M_E;
+            } else if (strcmp(name, "j") == 0) {
+                value_stack[++value_top] = unary_minus ? -I : I;
+            } else {
+                complex double value = get_variable_value(name);
+                if (isnan(creal(value)) || isnan(cimag(value))) {
+                    return NAN;
+                }
+                value_stack[++value_top] = unary_minus ? -value : value;
+            }
+            unary_minus = 0;
+        } else if (is_operator(expr[i])) {
+            char op = expr[i];
+            if (op == '-' && (i == 0 || is_operator(expr[i - 1]) || expr[i - 1] == '(')) {
+                unary_minus = 1;
+            } else {
+                while (operator_top >= 0 && is_operator(operator_stack[operator_top]) &&
+                       get_operator_precedence(op) <= get_operator_precedence(operator_stack[operator_top])) {
+                    complex double y = value_stack[value_top--];
+                    complex double x = value_stack[value_top--];
+                    char op2 = operator_stack[operator_top--];
+                    value_stack[++value_top] = apply_operator(op2, x, y);
+                }
+                operator_stack[++operator_top] = op;
+            }
+            i++;
+        } else if (expr[i] == '(') {
+            operator_stack[++operator_top] = expr[i];
+            i++;
+        } else if (expr[i] == ')') {
+            while (operator_top >= 0 && operator_stack[operator_top] != '(') {
+                complex double y = value_stack[value_top--];
+                complex double x = value_stack[value_top--];
+                char op = operator_stack[operator_top--];
+                value_stack[++value_top] = apply_operator(op, x, y);
+            }
+            if (operator_top < 0 || operator_stack[operator_top] != '(') {
+                printf("Mismatched parentheses\n");
+                return NAN;
+            }
+            operator_top--;
+            i++;
+        } else {
+            printf("Invalid character: %c\n", expr[i]);
+            return NAN;
+        }
+    }
+    while (operator_top >= 0) {
+        if (operator_stack[operator_top] == '(') {
+            printf("Mismatched parentheses\n");
+            return NAN;
+        }
+        complex double y = value_stack[value_top--];
+        complex double x = value_stack[value_top--];
+        char op = operator_stack[operator_top--];
+        value_stack[++value_top] = apply_operator(op, x, y);
+    }
+    if (value_top != 0 || operator_top != -1) {
+        printf("Invalid expression\n");
+        return NAN;
+    }
+    return value_stack[value_top];
+}
+
+int main() {
+    char expression[MAX_EXPR_LEN];
+    printf("Enter an expression: ");
+    fgets(expression, sizeof(expression), stdin);
+    complex double result = evaluate_expression(expression);
+    printf("Result: %.2f%+.2fi\n", creal(result), cimag(result));
     return 0;
 }
